8a2b2b2d10e46c3bde011f743a048a88279a4cb7
Jai Malani <71269258+jai38@users.noreply.github.com>
2021-10-06 22:49:59 +0530

    home page added

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.get("/", (req, res) => {
+  res.json({
+    Status: "200",
+    body: "All OK",
+    description:
+      "please send POST request to /email-verify with body {'email':'example@gmail.com'}",
+  });
+});
 app.use("/email-verify", require("./routes/email-verify"));
 app.listen(PORT, () => {
   console.log(`On Port ${PORT}`);
